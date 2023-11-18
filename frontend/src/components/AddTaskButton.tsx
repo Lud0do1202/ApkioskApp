bf9d30876b91d02d3b65ff7b67f392e0ba954e89
Lud0do1202 <ludo.traina@gmail.com>
2023-11-18 19:32:36 +0100

    Code cleanup + optimize imports

--- frontend/src/components/AddTaskButton.tsx
+++ frontend/src/components/AddTaskButton.tsx
@@ -1,25 +1,22 @@
-import React, { useRef, useState } from 'react'
-import { Box, Button, Container, IconButton, Modal, Paper, Typography, useTheme } from '@mui/material'
-import { Delete } from '@mui/icons-material'
-import { Task } from '../models/Task'
-import WarningAmberIcon from '@mui/icons-material/WarningAmber'
-import { CrudTasks } from '../models/CrudTasks'
-import { HandleOpenModalRef } from '../models/HandleOpenModalRef'
-import ModalDeleteTask from './ModalDeleteTask'
+import React, {useRef} from 'react'
+import {Button} from '@mui/material'
+import {Task} from '../models/Task'
+import {CrudTasks} from '../models/CrudTasks'
+import {HandleOpenModalRef} from '../models/HandleOpenModalRef'
 import ModalEditTask from './ModalEditTask'
 
-const AddTaskButton: React.FC<{ task: Task | undefined; crudTasks: CrudTasks }> = ({ task, crudTasks }) => {
-	const modalRef = useRef<HandleOpenModalRef>(null)
+const AddTaskButton: React.FC<{ task: Task | undefined; crudTasks: CrudTasks }> = ({task, crudTasks}) => {
+    const modalRef = useRef<HandleOpenModalRef>(null)
 
-	return (
-		<>
-			<Button variant="contained" color="primary" onClick={() => modalRef.current?.handleOpen()}>
-				Ajouter une tâche
-			</Button>
+    return (
+        <>
+            <Button variant="contained" color="primary" onClick={() => modalRef.current?.handleOpen()}>
+                Ajouter une tâche
+            </Button>
 
-			<ModalEditTask ref={modalRef} task={task} crudTasks={crudTasks} />
-		</>
-	)
+            <ModalEditTask ref={modalRef} task={task} crudTasks={crudTasks}/>
+        </>
+    )
 }
 
 export default AddTaskButton
--- frontend/src/components/DeleteTaskButton.tsx
+++ frontend/src/components/DeleteTaskButton.tsx
@@ -1,24 +1,23 @@
-import React, { useRef, useState } from 'react'
-import { Box, Button, Container, IconButton, Modal, Paper, Typography, useTheme } from '@mui/material'
-import { Delete } from '@mui/icons-material'
-import { Task } from '../models/Task'
-import WarningAmberIcon from '@mui/icons-material/WarningAmber'
-import { CrudTasks } from '../models/CrudTasks'
-import { HandleOpenModalRef } from '../models/HandleOpenModalRef'
+import React, {useRef} from 'react'
+import {IconButton} from '@mui/material'
+import {Delete} from '@mui/icons-material'
+import {Task} from '../models/Task'
+import {CrudTasks} from '../models/CrudTasks'
+import {HandleOpenModalRef} from '../models/HandleOpenModalRef'
 import ModalDeleteTask from './ModalDeleteTask'
 
-const DeleteTaskButton: React.FC<{ task: Task; crudTasks: CrudTasks }> = ({ task, crudTasks }) => {
-	const modalRef = useRef<HandleOpenModalRef>(null)
+const DeleteTaskButton: React.FC<{ task: Task; crudTasks: CrudTasks }> = ({task, crudTasks}) => {
+    const modalRef = useRef<HandleOpenModalRef>(null)
 
-	return (
-		<>
-			<IconButton aria-label="delete" color="error" onClick={() => modalRef.current?.handleOpen()}>
-				<Delete />
-			</IconButton>
+    return (
+        <>
+            <IconButton aria-label="delete" color="error" onClick={() => modalRef.current?.handleOpen()}>
+                <Delete/>
+            </IconButton>
 
-			<ModalDeleteTask ref={modalRef} task={task} crudTasks={crudTasks} />
-		</>
-	)
+            <ModalDeleteTask ref={modalRef} task={task} crudTasks={crudTasks}/>
+        </>
+    )
 }
 
 export default DeleteTaskButton
