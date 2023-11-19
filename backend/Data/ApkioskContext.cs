68b5a4b7d2f49344217e18de1125d8f415f3823a
Lud0do1202 <ludo.traina@gmail.com>
2023-11-19 12:54:23 +0100

    Init backend - Creation of the project backend with Visual Studio - Creation models  - User (id, lastname, firstname, avatar, {tasks})  - Task (id, label, userId, status, {user}) - Set relation context OnModelCreating - Creation base TasksController - Init users when Update-Database migration "init"

---
+++ backend/Data/ApkioskContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Data
+{
+    public class ApkioskContext : DbContext
+    {
+        public ApkioskContext (DbContextOptions<ApkioskContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<backend.Models.User> User { get; set; } = default!;
+
+        public DbSet<backend.Models.Task> Task { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // TASK >> User
+            modelBuilder.Entity<Models.Task>()
+                .HasOne(x => x.User)            // task     has 1..0    user
+                .WithMany(x => x.Tasks)         // user     has 0..*    tasks
+                .HasForeignKey(x => x.UserId);  // task     has FK on   UserId
+        }
+    }
+}
