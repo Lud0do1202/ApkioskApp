cc93bbbea40590e56504b9aebadf69af63434501
Lud0do1202 <ludo.traina@gmail.com>
2023-11-19 23:25:51 +0100

    Pie Chart - Update dashboard in order to implement future charts - Fetch tasks - Add Pie chart - LegendPieChart : The legend for the pie chart

---
+++ frontend/src/components/LegendPieChart.tsx
@@ -0,0 +1,27 @@
+import { Box, LinearProgress, Typography } from '@mui/material'
+import React from 'react'
+
+const LegendPieChart: React.FC<{
+	color: string
+	value: number
+	total: number
+	label: string
+}> = ({ color, value, total, label }) => {
+	const calc = Math.floor((value / total) * 1000) / 10
+
+	return (
+		<Box color={color}>
+			<Box display={'flex'} justifyContent={'space-between'}>
+				<Typography color={"inherit"} variant="caption">
+					{label}
+				</Typography>
+				<Typography color={"inherit"} variant="caption">
+					{calc}%
+				</Typography>
+			</Box>
+			<LinearProgress variant="determinate" color={"inherit"} value={calc} sx={{ height: 5, borderRadius: 20 }} />
+		</Box>
+	)
+}
+
+export default LegendPieChart
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import { Box } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import PieChartStatus from '../components/PieChartStatus'
+import { Task } from '../models/Task'
+import LoaderPage from '../components/LoaderPage'
 
 const DashboardPage: React.FC = () => {
-    return (<div>
-        <div>DashboardPage</div>
-    </div>)
+	// Fetch tasks
+	const [tasks, setTasks] = useState<Task[] | undefined>(undefined)
+	useEffect(() => {
+		fetch('https://localhost:7278/api/Tasks', { method: 'GET' })
+			.then((res) => res.json())
+			.then((tasksApi: Task[]) => setTasks(tasksApi))
+			.catch((e) => console.error(e))
+	}, [])
+
+	return (
+		<>
+			{tasks === undefined ? (
+				<LoaderPage />
+			) : (
+				<Box display={"flex"} justifyContent={"space-around"}>
+					<PieChartStatus tasks={tasks} />
+				</Box>
+			)}
+		</>
+	)
 }
 
-export default DashboardPage;
+export default DashboardPage
